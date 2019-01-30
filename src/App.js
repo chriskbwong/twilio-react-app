26c092beb4ec3a31e2fa40ed155c9177d16c25fb
Chris Wong <chriskbwong@gmail.com>
2019-01-30 11:10:21 -0800

    Added proxy server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,95 @@
-import React, { Component } from 'react'
-import MessageForm from './MessageForm'
-import MessageList from './MessageList'
-import './App.css'
+import React, { Component } from "react";
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import $ from "jquery";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       messages: [],
-    }
+      username: null,
+      channel: null
+    };
   }
 
-  handleNewMessage = (text) => {
+  componentDidMount = () => {
+    this.getToken()
+      .then(this.createChatClient)
+      .then(this.joinGeneralChannel)
+      .catch(error => {
+        this.addMessage({ body: `Error: ${error.message}` });
+      });
+  };
+
+  getToken = () => {
+    return new Promise((resolve, reject) => {
+      this.setState({
+        messages: [...this.state.messages, { body: `Connecting...` }]
+      });
+
+      $.getJSON("/token", token => {
+        this.setState({ username: token.identity });
+        resolve(token);
+      }).fail(() => {
+        reject(Error("Failed to connect."));
+      });
+    });
+  };
+
+  createChatClient = token => {
+    return new Promise((resolve, reject) => {
+      resolve(new TwilioChat(token.jwt));
+    });
+  };
+
+  joinGeneralChannel = chatClient => {
+    return new Promise((resolve, reject) => {
+      chatClient
+        .getSubscribedChannels()
+        .then(() => {
+          chatClient
+            .getChannelByUniqueName("general")
+            .then(channel => {
+              this.addMessage({ body: "Joining general channel..." });
+              this.setState({ channel });
+
+              channel
+                .join()
+                .then(() => {
+                  this.addMessage({
+                    body: `Joined general channel as ${this.state.username}`
+                  });
+                  window.addEventListener("beforeunload", () =>
+                    channel.leave()
+                  );
+                })
+                .catch(() => reject(Error("Could not join general channel.")));
+
+              resolve(channel);
+            })
+            .catch(() => reject(Error("Could not find general channel.")));
+        })
+        .catch(() => reject(Error("Could not get channel list.")));
+    });
+  };
+
+  handleNewMessage = text => {
     this.setState({
-      messages: [...this.state.messages, { me: true, author: "Me", body: text }],
-    })
-  }
+      messages: [...this.state.messages, { me: true, author: "Me", body: text }]
+    });
+  };
+
+  addMessage = message => {
+    const messageData = {
+      ...message,
+      me: message.author === this.state.username
+    };
+    this.setState({
+      messages: [...this.state.messages, messageData]
+    });
+  };
 
   render() {
     return (
@@ -23,8 +97,8 @@ class App extends Component {
         <MessageList messages={this.state.messages} />
         <MessageForm onMessageSend={this.handleNewMessage} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
