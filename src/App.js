04667d3fc8585dd9dcc9e85b43b064cf01a50f16
Chris Wong <chriskbwong@gmail.com>
2019-02-02 22:01:42 -0800

    pivot to philnash
    https://github.com/philnash/twilio-chat-kendo-react
    this is now a fork of philnash's twilio, react, and kendo based chat application

--- src/App.js
+++ src/App.js
@@ -1,125 +1,44 @@
-import React, { Component } from "react";
-import MessageForm from "./MessageForm";
-import MessageList from "./MessageList";
-import TwilioChat from "twilio-chat";
-import $ from "jquery";
-import "./App.css";
+import React, { Component } from 'react';
+import Login from './Login';
+import ChatApp from './ChatApp';
+import '@progress/kendo-theme-material/dist/all.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: [],
-      username: null,
-      channel: null
+      username: '',
+      loggedIn: false
     };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
   }
 
-  componentDidMount = () => {
-    this.getToken()
-      .then(this.createChatClient)
-      .then(this.joinGeneralChannel)
-      .then(this.configureChannelEvents)
-      .catch(error => {
-        this.addMessage({ body: `Error: ${error.message}` });
-      });
-  };
 
-  getToken = () => {
-    return new Promise((resolve, reject) => {
-      this.addMessage({ body: "Connecting..." });
-
-      $.getJSON("/token", token => {
-        this.setState({ username: token.identity });
-        resolve(token);
-      }).fail(() => {
-        reject(Error("Failed to connect."));
-      });
-    });
-  };
-
-  createChatClient = token => {
-    return new Promise((resolve, reject) => {
-      resolve(new TwilioChat(token.jwt));
-    });
-  };
-
-  joinGeneralChannel = chatClient => {
-    return new Promise((resolve, reject) => {
-      chatClient
-        .getSubscribedChannels()
-        .then(() => {
-          chatClient
-            .getChannelByUniqueName("general")
-            .then(channel => {
-              this.addMessage({ body: "Joining general channel..." });
-              this.setState({ channel });
-
-              channel
-                .join()
-                .then(() => {
-                  this.addMessage({
-                    body: `Joined general channel as ${this.state.username}`
-                  });
-                  window.addEventListener("beforeunload", () =>
-                    channel.leave()
-                  );
-                })
-                .catch(() => reject(Error("Could not join general channel.")));
-
-              resolve(channel);
-            })
-            .catch(() => this.createGeneralChannel(chatClient));
-        })
-        .catch(() => reject(Error("Could not get channel list.")));
-    });
-  };
-
-  createGeneralChannel = chatClient => {
-    return new Promise((resolve, reject) => {
-      this.addMessage({ body: "Creating general channel..." });
-      chatClient
-        .createChannel({ uniqueName: "general", friendlyName: "General Chat" })
-        .then(() => this.joinGeneralChannel(chatClient))
-        .catch(() => reject(Error("Could not create general channel.")));
-    });
-  };
-
-  addMessage = message => {
-    const messageData = {
-      ...message,
-      me: message.author === this.state.username
-    };
-    this.setState({
-      messages: [...this.state.messages, messageData]
-    });
-  };
-
-  handleNewMessage = text => {
-    if (this.state.channel) {
-      this.state.channel.sendMessage(text);
-    }
-  };
-
-  configureChannelEvents = channel => {
-    channel.on("messageAdded", ({ author, body }) => {
-      this.addMessage({ author, body });
-    });
-
-    channel.on("memberJoined", member => {
-      this.addMessage({ body: `${member.identity} has joined the channel.` });
-    });
-
-    channel.on("memberLeft", member => {
-      this.addMessage({ body: `${member.identity} has left the channel.` });
-    });
-  };
+  handleLogin(event) {
+    event.preventDefault();
+    this.setState({ loggedIn: true });
+  }
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
 
   render() {
+    let loginOrChat;
+    if (this.state.loggedIn) {
+      loginOrChat = <ChatApp username={this.state.username}/>;
+    } else {
+      loginOrChat = (
+        <Login
+          handleLogin={this.handleLogin}
+          handleUsernameChange={this.handleUsernameChange}
+          username={this.state.username}
+        />
+      );
+    }
     return (
-      <div className="App">
-        <MessageList messages={this.state.messages} />
-        <MessageForm onMessageSend={this.handleNewMessage} />
+      <div className="container">
+        <div className="row mt-3 justify-content-center">{loginOrChat}</div>
       </div>
     );
   }
